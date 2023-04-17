c3ed585d2aba30944ac7c50965f520424c5ea146
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-04-17 21:51:44 +0530

    Add styles to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,15 @@ import './styles/Navbar.css';
 const Navbar = () => {
   return (
     <nav>
+      <div className='display-logo'>
       <img src={logo} alt="logo" />
-      <div className="navigation">
-        <h1>Space Travellers Hub</h1>
-        <ul>
-          <li><NavLink to="/">Rockets</NavLink></li>
-          <li><NavLink to="/mission">Mission</NavLink></li>
-          <li><NavLink to="/profile">MyProfile</NavLink></li>
-        </ul>
+      <h1>Space Traveler's Hub</h1>
       </div>
+        <ul className="navigation">
+          <li className='rockets'><NavLink to="/">Rockets</NavLink></li>
+          <li className='mission'><NavLink to="/mission">Mission</NavLink></li>
+          <li className='profile-link'><NavLink to="/profile">My Profile</NavLink></li>
+        </ul>
     </nav>
   )
 }
