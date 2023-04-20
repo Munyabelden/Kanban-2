36ee81c944166d99f1b9ade3c4d53f050aecdf8c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-04-20 19:13:48 +0530

    Add some styles to Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import "./styles/Profile.css";
 const Profile = () => {
   const { missions } = useSelector((state) => state.missions);
   const joinedMembers = missions.filter((mission) => mission.joined);
   const { rockets } = useSelector((state) => state.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
 
-  if (!joinedMembers) {
-    return <h3>There are no active members</h3>;
-  }
-
   return (
-    <div>
-      <div>
+    <div className="profile-container">
+      <div className="missions-display">
         <h2>My Missions</h2>
-        {joinedMembers.map((mission) => {
-          return <li>{mission.mission_name}</li>;
-        })}
+        {joinedMembers.length === 0 ? (
+          <h3>There are no active members</h3>
+        ) : (
+          joinedMembers.map((mission) => {
+            return <li>{mission.mission_name}</li>;
+          })
+        )}
       </div>
-      <div>
+      <div className="rockets-display">
         <h2>My Reserved Rockets</h2>
         {reservedRockets.length === 0 ? (
           <h3>You haven't reserved any rockets yet</h3>
