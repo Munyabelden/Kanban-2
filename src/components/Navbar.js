e836352665f73572604188ec04d251a55ce5fe70
Gracerue <munyaradzi045@gmail.com>
2023-04-20 21:13:34 +0200

    Make changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import logo from '../assets/planet.png';
 import './styles/Navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav>
       <div className='display-logo'>
-      <img src={logo} alt="logo" />
-      <h1>Space Traveler's Hub</h1>
+        <img src={logo} alt="logo" />
+        <h1>Space Traveler's Hub</h1>
       </div>
-        <ul className="navigation">
-          <li className='rockets'><NavLink to="/">Rockets</NavLink></li>
-          <li className='mission'><NavLink to="/mission">Mission</NavLink></li>
-          <li className='profile-link'><NavLink to="/profile">My Profile</NavLink></li>
-        </ul>
+      <ul className="navigation">
+        <li className='rockets'>
+          <NavLink to="/" isActive={() => location.pathname === '/'}>Rockets</NavLink>
+        </li>
+        <li className='mission'>
+          <NavLink to="/mission" isActive={() => location.pathname === '/mission'}>Mission</NavLink>
+        </li>
+        <li className='profile-link'>
+          <NavLink to="/profile" isActive={() => location.pathname === '/profile'}>My Profile</NavLink>
+        </li>
+      </ul>
     </nav>
   )
 }
