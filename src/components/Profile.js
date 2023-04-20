edcc564221b5ea1bb892109275648b0cd539fddf
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-04-20 18:50:44 +0530

    Add test for rocketSlice and add reserved rocket in profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const { missions } = useSelector(state => state.missions);
-  const joinedMembers = missions.filter(mission => mission.joined);
+  const { missions } = useSelector((state) => state.missions);
+  const joinedMembers = missions.filter((mission) => mission.joined);
+  const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
 
-  if(! joinedMembers){
-    return (<h3>There are no active members</h3>)
+  if (!joinedMembers) {
+    return <h3>There are no active members</h3>;
   }
-  
+
   return (
     <div>
       <div>
         <h2>My Missions</h2>
-        {joinedMembers.map(mission => {
-          return (
-            <li>{mission.mission_name}</li>
-          )
+        {joinedMembers.map((mission) => {
+          return <li>{mission.mission_name}</li>;
         })}
+      </div>
+      <div>
+        <h2>My Reserved Rockets</h2>
+        {reservedRockets.length === 0 ? (
+          <h3>You haven't reserved any rockets yet</h3>
+        ) : (
+          reservedRockets.map((rocket) => {
+            return <li>{rocket.name}</li>;
+          })
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Profile;
